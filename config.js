882aee2a490b5ecb161e6d0ad18144800c5cc18e
EK Falcon <32907812+Eudes1857@users.noreply.github.com>
2021-10-08 13:35:03 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,22 @@
 const Pool = require('pg').Pool
+require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
 const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+/*const pool = new Pool({
   user: 'eudes',
   host: 'localhost',
   database: 'db_boutique',
   password: '1857',
   port: 5432,
-})
+})*/
 
 const getProducts = (request, response) => {
     pool.query('SELECT * FROM products ORDER BY idproduct ASC', (error, results) => {
